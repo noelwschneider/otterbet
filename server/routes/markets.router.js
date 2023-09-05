7e0d9bbc35e03d6c599968dd932e0101ad419d47
noelwschneider <noelwschneider@gmail.com>
2023-09-05 17:25:29 -0500

    added code to markets saga to get all markets for each game from the database

--- server/routes/markets.router.js
+++ server/routes/markets.router.js
@@ -1,3 +1,5 @@
+// This router deals with market data already in the database
+
 const express = require('express');
 const axios = require('axios')
 
@@ -9,163 +11,63 @@ const pool = require('../modules/pool');
 const userStrategy = require('../strategies/user.strategy');
 
 const router = express.Router();
-const oddsAPIKey = process.env.ODDS_API_KEY
-
-//& This may be better off in its own file (esp if it may be useful when dealing with other requests from odds-api)
-const fixTimestamp = timestamp => {
-    let fixedTimestamp = []
-    for (let character of timestamp) {
-        if (character === 'T') {
-            fixedTimestamp.push(' ')
-        } else if (character !== 'Z') {
-            fixedTimestamp.push(character)
-        }
-    }
-    return fixedTimestamp.join('')
-}
-
-//& This should live elsewhere and be required into this
-function makeMarketsArray(apiData) {
-    let arrayToReturn = []
-    for (let game of apiData) {
-        for (let bookmaker of game.bookmakers) {
-            for (let market of bookmaker.markets) {
-                for (let outcome of market.outcomes) {
-                    let marketObj = {
-                        game_id: game.id,
-                        bookmaker: bookmaker.key,
-                        market: market.key,
-                        outcome: outcome.name,
-                        price: outcome.price,
-                        point: outcome.point,
-                        last_update: fixTimestamp(market.last_update)
-                    }
-                    arrayToReturn.push(marketObj)
-                }
-            }
-        }
-    }
-    return arrayToReturn
-}
-
-//& This should live elsewhere and be required into this
-function makeGamesArray(apiData) {
-    let arrayToReturn = []
-    for (let game of apiData) {
-        let gameObj = {
-            id: game.id,
-            home_team: game.home_team,
-            away_team: game.away_team,
-            commence_time: fixTimestamp(game.commence_time),
-            competition: game.sport_key
-        }
-        arrayToReturn.push(gameObj)
-    }
-    return arrayToReturn
-}
-
-// GET odds from database
 
 
-// GET odds data from API request and send to the reducer
-//! Add reject unauthenticated when this is working?
-router.get('/update-odds', (req, res) => {
+// GET list of game IDs from database
+router.get('/game-IDs', (req, res) => {
 
-    //& The odds reducer may eventually include several variables that determine the specifics of this request
-    //& bookmakers should eventually be replaced with regions
-    axios.get(`https://api.the-odds-api.com/v4/sports/upcoming/odds/?sport=americanfootball_nfl&markets=h2h,spreads,totals&bookmakers=betmgm&apiKey=${oddsAPIKey}`)
-        .then(response => {
-            console.log('response:', response.data)
+    console.log('in /markets/game-IDs GET')
+    // get IDs from Games
 
-            //& The games array should have its own route -- it won't need to be updated all that frequently
-            let marketsArray = makeMarketsArray(response.data)
-
-            //* long term, I need a way to avoid redundant data (i.e. don't add a row to the database if there is already an identical row because nothing changed since the last update)
-
-            res.send(marketsArray)
-        })
-        .catch(error => {
-            console.log('error in odds router:', error)
-        })
-})
-
-// POST odds from store to the database
-router.post('/', (req, res) => {
-
-    let queryText = `
-        INSERT INTO markets (
-            bookmaker, 
-            game_id, 
-            outcome, 
-            market, 
-            point, 
-            price, 
-            last_update
-        )
-        VALUES ($1, $2, $3, $4, $5, $6, $7)
-    `
-
-    req.body.map( newMarket => {
-        const { game_id, bookmaker, market, outcome, price, point, last_update } = newMarket
-        let queryData = [bookmaker, game_id, outcome, market, point, price, last_update]
-
-        pool.query(queryText, queryData)
-        .then(response => {
-            console.log('successfull market post:', queryData)
-        })
-        .catch(error => {
-            console.log('error in pool query:', error)
-            res.sendStatus(500)
-        })
-    })
-    res.sendStatus(200)
-})
-
-router.get('/update-games', (req, res) => {
-    //& The odds reducer may eventually include several variables that determine the specifics of this request
-    //& bookmakers should eventually be replaced with regions
-    axios.get(`https://api.the-odds-api.com/v4/sports/upcoming/odds/?sport=americanfootball_nfl&markets=h2h,spreads,totals&bookmakers=betmgm&apiKey=${oddsAPIKey}`)
-        .then(response => {
-            console.log('response:', response.data)
-
-            //& The games array should have its own route -- it won't need to be updated all that frequently
-            let gamesArray = makeGamesArray(response.data)
-
-            //* long term, I need a way to avoid redundant data (i.e. don't add a row to the database if there is already an identical row because nothing changed since the last update)
-
-            res.send(gamesArray)
-        })
-        .catch(error => {
-            console.log('error in odds router:', error)
-        })
-})
-
-router.post('/games', (req, res) => {
-    console.log('games post req:', req.body)
-
-    let queryText = `
-        INSERT INTO games (id, home_team, away_team, commence_time, competition)
-        VALUES ($1, $2, $3, $4, $5)
-    `
-
-    req.body.map( game => {
-        const {id, home_team, away_team, commence_time, competition} = game
-        let queryData = [id, home_team, away_team, commence_time, competition]
-
-        pool.query(queryText, queryData)
-        .then( response => {
-            console.log('successful game post', queryData)
-        })
-        .catch( error => {
-            console.log('error in pool query:', error)
-            res.sendStatus(500)
-        })
-    })
-
-    res.sendStatus(200)
-    // const [test] = req.body
-    // const {id, home_team, away_team, commence_time, competition} = test
+    //& timestamps in the WHERE sections should be variables later
+    const gameIDsQuery = `
+    SELECT id 
+    FROM games
+    WHERE commence_time BETWEEN 
+        timestamp '2023-09-08' 
+        AND timestamp '2023-09-15'
+    ;`
     
+    // GET list of game IDs from database
+    pool.query(gameIDsQuery)
+    .then( response => {
+        console.log('database response:', response.rows)
+        res.send(response.rows)
+    })
+
+    // get all Markets rows that match each game ID
+    // put each set of markets into an object
+    // send back an array of those objects
+})
+
+// GET all markets for each game
+router.get('/', async (req, res) => {
+    console.log('in /markets GET')
+    // console.log('req.query:', req.query)
+
+    //& As is, this arrives as a JSON object. There might be a way to avoid the JSON.parse() by sending it up differently.
+    const gamesList = req.query.gamesList.map(game => JSON.parse(game))
+    // console.log('game list:', gamesList)
+
+
+
+    const markets = await Promise.all(gamesList.map( async (game) => {
+        const queryValue = [game.id ]
+        console.log('query value:', queryValue)
+
+        const queryText = `
+            SELECT *
+            FROM markets
+            WHERE game_id = $1
+        `
+
+        const response = await pool.query(queryText, queryValue) 
+        // console.log('response from DB:', response.rows)
+        return response.rows
+        })
+    )
+    console.log(markets)
+    res.send(markets)
 })
 
 module.exports = router;
--- src/redux/sagas/markets.saga.js
+++ src/redux/sagas/markets.saga.js
@@ -1,8 +1,32 @@
 import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
+// Saga for getting array of markets for each game
+function* getMarketsArray() {
+    try {
+        //& I use this like 50 times, should I just put it somewhere else and import it?
+        const config = {
+            headers: { 
+                'Content-Type': 'application/json',
+        },
+            withCredentials: true,
+            params: {}
+        };
+
+        const gameIDsResponse = yield axios.get('/api/markets/game-IDs', config)
+        //& This is sending up an array of JSON strings. I can work with that, but it is worth investigating if this is really the way to do this
+        config.params = {gamesList: gameIDsResponse.data}
+
+        const marketsList = yield axios.get('/api/markets', config)
+        console.log(marketsList.data)
+
+    } catch (error) {
+        console.log('error in markets.saga:', error)
+    }
+}
+
 function* marketsSaga() {
-    // yield takeLatest, etc.
+    yield takeLatest('FETCH_MARKETS', getMarketsArray)
 }
 
 export default marketsSaga
