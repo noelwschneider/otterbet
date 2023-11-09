7fe186aad9408c239dafe7b2f6a9c5fca2529b8d
noelwschneider <noelwschneider@gmail.com>
2023-11-09 12:17:29 -0600

    white space improvements

--- server/routes/contests/contests.router.js
+++ server/routes/contests/contests.router.js
@@ -1,31 +1,23 @@
 const express = require('express');
-const {
-  rejectUnauthenticated,
-} = require('../../modules/authentication-middleware');
-const encryptLib = require('../../modules/encryption');
 const pool = require('../../modules/pool');
-const userStrategy = require('../../strategies/user.strategy');
-
 const router = express.Router();
 
-router.post('/', (req, res) => {
 
-    let placeholderString = ''
+router.post('/', (req, res) => {
+    let placeholderString = '';
     let counter = 1;
     let colsArray = [];
     let queryValues = [];
     for (let property in req.body) {
         colsArray.push(property)
         queryValues.push(req.body[property]);
-        
-        placeholderString = placeholderString + `$${counter},`
+        placeholderString = placeholderString + `$${counter},`;
         counter++;
     }
     placeholderString = placeholderString.slice(0, -1); // Removing last comma
     
     let colsString = '"' + colsArray.join(`", "`) + '"';
 
-    //! Does this need sanitizing? Need to look into it
     const queryText = `
         INSERT INTO contests (
             ${colsString}
